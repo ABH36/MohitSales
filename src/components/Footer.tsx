7e81a31a82f12a58d50d6bdf71333a7b32264fdf
Shishank Soni <141385714+Shishanksoni906@users.noreply.github.com>
2026-06-30 14:55:25 +0530

    chore: change BDM footer logo to use original bdm_footer.png colored logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -200,7 +200,7 @@ export default function Footer() {
                       © <span>2026</span> Mohit Sales Corporation Pvt. Ltd. Designed by{' '}
                       <a href="https://www.bdminfotech.com/" target="_blank" rel="noopener noreferrer">
                         <img
-                          src="/assets/images/logo/bdm_cloudtech_white.png"
+                          src="/assets/images/logo/bdm_footer.png"
                           className="bdm-footer-logo inline-block align-middle ml-1"
                           alt="BDM Cloudtech"
                           style={{ display: 'inline-block', verticalAlign: 'middle', height: '28px', width: 'auto' }}
